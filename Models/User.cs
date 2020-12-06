11b5b9294d70e611fa21f945216d8f23636a51f9
omar-geberi <omar.geberi@gmail.com>
2020-12-06 07:01:21 +0200

    InitialCommit

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dotnet_user.Models
+{
+    public class User
+    {
+        public int nId { get; set; }
+        public String name { get; set; }
+        public string mobileNo { get; set; }
+        
+    }
+}
